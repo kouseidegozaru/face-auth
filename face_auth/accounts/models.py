d4975c7301ee4ef6c1a3b89764c2d6698c306ed0
kouseidegozaru <gguangchui@gmail.com>
2024-09-18 12:07:15 +0900

    ユーザーモデルのスーパーユーザーに関する設定を明示的に宣言

--- face_auth/accounts/models.py
+++ face_auth/accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 import uuid
 
@@ -23,32 +23,31 @@ class UserManager(BaseUserManager):
             password=password,
         )
         user.is_admin = True
+        user.is_staff = True  # スタッフ権限を明示
+        user.is_superuser = True  # スーパーユーザー権限を設定
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(verbose_name='メールアドレス', max_length=255, unique=True)
     name = models.CharField(verbose_name='名前', max_length=100)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'  # ログイン時に必要なフィールド
-    REQUIRED_FIELDS = ['name'] # ユーザー作成時に必要なフィールド
+    USERNAME_FIELD = 'email'  # ログイン時に使用するフィールド
+    REQUIRED_FIELDS = ['name']  # ユーザー作成時に必要なフィールド
 
     def __str__(self):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        return True
+        return self.is_superuser  # スーパーユーザー権限がある場合のみTrue
 
     def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
+        return self.is_superuser  # スーパーユーザー権限がある場合のみTrue
