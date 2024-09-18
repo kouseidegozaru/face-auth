7effea63e196e27239f1bcf42e9b9b60605c0413
kouseidegozaru <gguangchui@gmail.com>
2024-09-18 14:53:35 +0900

    ユーザー登録メールが送信できない問題を解決

--- face_auth/accounts/urls.py
+++ face_auth/accounts/urls.py
@@ -3,5 +3,6 @@ from .views import CustomLoginView, CustomRegisterView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='custom_login'),
     path('registration/', CustomRegisterView.as_view(), name='custom_registration'),
+    path('registration/', include('dj_rest_auth.registration.urls')),
     path('', include('dj_rest_auth.urls')),
 ]
