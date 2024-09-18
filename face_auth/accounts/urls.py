ad79099f632352da11061280c023e664566e7c3d
kouseidegozaru <gguangchui@gmail.com>
2024-09-18 11:59:08 +0900

    ログイン用のviewとルーティングを設定

--- face_auth/accounts/urls.py
+++ face_auth/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='custom_login'),
     path('', include('dj_rest_auth.urls')),
-    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls'))
 ]
