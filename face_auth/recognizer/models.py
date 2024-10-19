3b3706649b5344dc1abc477f7bbd2e8b5fba4bae
kouseidegozaru <gguangchui@gmail.com>
2024-10-19 10:48:19 +0900

    学習グループモデルの作成

--- face_auth/recognizer/models.py
+++ face_auth/recognizer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
+from accounts.models import User
 
-# Create your models here.
+class TrainingGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='名前', max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
