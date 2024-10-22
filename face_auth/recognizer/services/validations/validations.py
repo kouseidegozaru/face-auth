a24997283b7be2cfa4beb17026b8e449841f60bf
kouseidegozaru <gguangchui@gmail.com>
2024-10-22 09:57:21 +0900

    入力画像が顔を含むかのバリデーションを行う関数の実装

---
+++ face_auth/recognizer/services/validations/validations.py
@@ -0,0 +1,13 @@
+from ..learning import detect_face
+import numpy as np
+
+def is_exist_face(image : np.ndarray) -> bool:
+    """
+    入力された画像が顔画像の場合にTrueを返す
+    """
+    face_image = detect_face(image)
+    if face_image is None:
+        return False
+    else:
+        return True
+    
