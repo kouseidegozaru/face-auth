31dcd396166061325e8c8140e6543b102da72756
kouseidegozaru <gguangchui@gmail.com>
2024-10-25 10:14:58 +0900

    推論するコードを見やすく修正

--- face_auth/recognizer/views/logic/predict.py
+++ face_auth/recognizer/views/logic/predict.py
@@ -4,6 +4,9 @@ from ...services.learning.feature_models import FeatureModel, FeatureModelStorag
 
 import numpy as np
 
+"""
+特徴モデルの読み込み
+"""
 def feature_model_from_binary(model_binary: bytes) -> FeatureModel:
 
     # 特徴モデルをバイナリ形式から復元
@@ -13,9 +16,13 @@ def feature_model_from_binary(model_binary: bytes) -> FeatureModel:
 
 def feature_predict(training_group_id: int, image : np.ndarray) -> str:
 
+    # 学習するグループ
+    training_group = TrainingGroup.objects.get(id=training_group_id)
     # 特徴モデルを読み込む
-    feature_model_binary = FeatureData.objects.get(group_id=training_group_id).feature
+    feature_model_binary = FeatureData.objects.get(group=training_group).feature
     # 特徴モデルを復元
     feature_model = feature_model_from_binary(feature_model_binary)
-
-    return predict_feature(feature_model, image)
+    # 推論
+    result_label = predict_feature(feature_model, image)
+    
+    return result_label
