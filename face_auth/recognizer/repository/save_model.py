aa8a6f16ea57de68939b59cdc641abb134774178
kouseidegozaru <gguangchui@gmail.com>
2024-10-25 16:58:42 +0900

    学習機能の処理の切り分け

--- face_auth/recognizer/repository/save_model.py
+++ face_auth/recognizer/repository/save_model.py
@@ -1,5 +1,4 @@
 from ..models import TrainingGroup, FeatureData
-from ..services.recognize import create_training_data_set, learning
 from ..services.recognize.feature_models import FeatureModel, FeatureModelStorage
 
 
@@ -24,14 +23,3 @@ def save_feature_model(feature_model : FeatureModel, group : TrainingGroup):
     # データを更新
     feature_file.feature = binary_feature
     feature_file.save()
-
-def feature_training(training_group_id):
-    # 学習に必要なデータセットを作成
-    train_data_set = create_training_data_set(training_group_id)
-
-    # 学習
-    feature_model = learning(train_data_set)
-
-    # 学習後の特徴モデルを保存
-    save_feature_model(feature_model, train_data_set.group)
-    
--- face_auth/recognizer/views/recognize_views.py
+++ face_auth/recognizer/views/recognize_views.py
@@ -5,9 +5,10 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import TrainingGroup
-from ..repository import feature_training,feature_predict
+from ..repository import save_model,feature_predict,create_training_data_set
 from ..serializers import TrainSerializer, PredictSerializer
 from ..services.tools import open_image
+from ..services.recognize import train_feature
 
 class TrainView(APIView):
     # 認証済みのユーザーのみアクセス可能
@@ -26,8 +27,12 @@ class TrainView(APIView):
         if group.owner != request.user:
             return Response({"detail": "You do not have permission to train this group."}, status=status.HTTP_403_FORBIDDEN)
 
+        # データセットの作成
+        dataset = create_training_data_set(group.id)
         # 学習
-        feature_training(group.id)
+        feature_model = train_feature(dataset)
+        # モデルの保存
+        save_model(feature_model, group.id)
         
         return Response(status=status.HTTP_200_OK)
 
