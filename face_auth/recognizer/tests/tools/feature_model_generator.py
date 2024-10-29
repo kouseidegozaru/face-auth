74b6bea099a2811098c4ba5ca95e3a4401161cbc
kouseidegozaru <gguangchui@gmail.com>
2024-10-29 17:22:13 +0900

    テスト用の特徴モデルをランダムに生成するように変更

--- face_auth/recognizer/tests/tools/feature_model_generator.py
+++ face_auth/recognizer/tests/tools/feature_model_generator.py
@@ -1,27 +1,27 @@
 from ...services.recognize.feature_models import FeatureModelStorage, FeatureModel
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+import random
 
-def get_pre_feature_model() -> FeatureModel:
+def get_random_feature_model() -> FeatureModel:
     """
     テスト用の特徴モデルを生成
     """
     pre_trained_model = NearestNeighbors()
-    pre_trained_model.fit(np.array([1,2,3,4,5,6,7,8,9,10]))
-    pre_labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    pre_trained_model.fit(np.array([random.randint(0, 100)() for i in range(10)]))
+    pre_labels = [str(i) for i in range(10)]
     feature_model = FeatureModel(pre_trained_model, pre_labels)
     return feature_model
 
-def get_pre_binary_feature_model() -> bytes:
+def convert_to_binary_feature_model(feature_model: FeatureModel) -> bytes:
     """
-    テスト用の特徴モデルをバイナリ形式で生成
+    テスト用の特徴モデルをバイナリ形式に変換
     """
-    feature_model = get_pre_feature_model()
     feature_model_storage = FeatureModelStorage()
     feature_model_storage.set_model(feature_model)
     return feature_model_storage.convert_to_binary()
 
-def convert_pre_binary_feature_model_to_feature_model(binary_feature_model: bytes) -> FeatureModel:
+def convert_bynary_to_feature_model(binary_feature_model: bytes) -> FeatureModel:
     """
     テスト用の特徴モデルをバイナリ形式から特徴モデルに変換
     """
