dbf95259f512acec1afa847433529949321057fe
kouseidegozaru <gguangchui@gmail.com>
2024-10-31 11:58:17 +0900

    テスト用の画像の生成方法をSimpleUploadedFileに変更

--- face_auth/recognizer/tests/models/test_models_TrainingData.py
+++ face_auth/recognizer/tests/models/test_models_TrainingData.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from recognizer.models import TrainingData, TrainingGroup
-from ..tools.image_generator import generate_random_image
+from django.core.files.uploadedfile import SimpleUploadedFile
 import os
 
 class TestTrainingData(TestCase):
@@ -12,20 +12,25 @@ class TestTrainingData(TestCase):
             password='test_password',
         )
         self.group = TrainingGroup.objects.create(name='test_group', owner=self.user)
-        self.random_image = generate_random_image()
+
+        self.random_image = SimpleUploadedFile(
+            "test_image.jpg", b"random_image_data", content_type="image/jpeg"
+        )
+        
         self.training_data = TrainingData.objects.create(
             group=self.group,
             image=self.random_image,
             label='test_label'
         )
-        self.image_path = self.training_data.image.path
+        
+        # 削除対象のファイルパスリスト
+        self.image_paths = [self.training_data.image.path]
 
     def tearDown(self):
-        # テスト終了後に生成された画像ファイルを削除
-        if self.training_data.image:
-            if self.image_path:
-                if os.path.isfile(self.image_path):
-                    os.remove(self.image_path)
+        # テスト終了後に生成されたすべての画像ファイルを削除
+        for path in self.image_paths:
+            if os.path.isfile(path):
+                os.remove(path)
 
     def test_create(self):
         self.assertEqual(TrainingData.objects.count(), 1)
@@ -37,10 +42,10 @@ class TestTrainingData(TestCase):
         self.assertEqual(self.training_data.group, self.group)
 
     def test_image(self):
-        self.assertEqual(self.training_data.image.name, self.random_image.name)
+        self.assertEqual(self.training_data.image.name, "test_image.jpg")
 
     def test_image_path(self):
-        self.assertEqual(self.training_data.image.path, self.image_path)
+        self.assertEqual(self.training_data.image.path, self.image_paths[0])
 
     def test_label(self):
         self.assertEqual(self.training_data.label, 'test_label')
@@ -51,10 +56,15 @@ class TestTrainingData(TestCase):
         self.assertEqual(self.training_data.label, 'updated_label')
 
     def test_update_image(self):
-        updated_image = generate_random_image("updated_image.png")
+        updated_image = SimpleUploadedFile(
+            "updated_image.jpg", b"updated_image_data", content_type="image/jpeg"
+        )
         self.training_data.image = updated_image
         self.training_data.save()
-        self.assertEqual(self.training_data.image.name, "updated_image.png")
+        
+        # 更新後の画像パスをリストに追加
+        self.image_paths.append(self.training_data.image.path)
+        self.assertEqual(self.training_data.image.name, "updated_image.jpg")
 
     def test_delete(self):
         self.training_data.delete()
