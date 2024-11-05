e56b1550fd13a4312b16c4fbbba4df0d7737f08e
kouseidegozaru <gguangchui@gmail.com>
2024-11-05 11:46:05 +0900

    PredictViewのテストコードの修正

--- face_auth/recognizer/tests/views/test_views_Predict.py
+++ face_auth/recognizer/tests/views/test_views_Predict.py
@@ -3,13 +3,11 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
-from recognizer.models import TrainingGroup, TrainingData
+from recognizer.models import TrainingGroup, TrainingData, FeatureData
+from recognizer.tests.tools.feature_model_generator import get_random_feature_model, convert_to_binary_feature_model
 import os
 import uuid
-# 顔画像であるかをクリアするためのモックを作成
 from unittest.mock import patch
-
-"""PredictViewのテスト"""
 
 
 class TestPredictView(APITestCase):
@@ -20,20 +18,7 @@ class TestPredictView(APITestCase):
             name='test_user',
             password='test_password',
         )
-        # テスト用のTrainingGroupの作成
-        self.group = TrainingGroup.objects.create(name='test_group', owner=self.user)
-        # テスト用のTrainingDataの作成
-        self.data = TrainingData.objects.create(
-            label=' test_data',
-            group=self.group,
-            image=SimpleUploadedFile(
-                "test_image.jpg", b"random_image_data", content_type="image/jpeg"
-            )
-        )
-
-        # 削除対象のファイルパスを保持するリスト
-        self.image_paths = [self.data.image.path]
-
+        
         # 認証トークンの取得
         response = self.client.post(
             reverse('token_obtain_pair'),
@@ -44,8 +29,23 @@ class TestPredictView(APITestCase):
         if self.token is None:
             raise ValueError('token is None')
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+        # テスト用のTrainingGroupの作成
+        self.group = TrainingGroup.objects.create(name='test_group', owner=self.user)
         
-        self.url = reverse('predict', args=[self.data.pk])
+        # テスト用の特徴モデルの作成
+        self.feature_model = get_random_feature_model()
+        self.binary_feature_model = convert_to_binary_feature_model(self.feature_model)
+        # テスト用のFeatureDataの作成
+        self.feature_data = FeatureData.objects.create(
+            group=self.group,
+            feature=self.binary_feature_model,
+        )
+
+        # 推論する画像の作成
+        self.image = SimpleUploadedFile(
+            "test_image.jpg", b"random_image_data", content_type="image/jpeg"
+        )
 
     def tearDown(self):
         # ファイルの削除
@@ -56,23 +56,43 @@ class TestPredictView(APITestCase):
     @patch('recognizer.service.validations.validations.is_exist_face', return_value=True)
     def test_post(self):
         # POSTリクエストのテスト
-        # TrainingDataを2つ作成
-        TrainingData.objects.create(
-            label=' test_data',
-            group=self.group,
-            image=SimpleUploadedFile(
-                "test_image.jpg", b"random_image_data", content_type="image/jpeg"
-            )
-        )
-        TrainingData.objects.create(
-            label='test_data2',
-            group=self.group,
-            image=SimpleUploadedFile(
-                "test_image2.jpg", b"random_image_data", content_type="image/jpeg"
-            )
-        )
-        response = self.client.post(self.url)
+        self.url = reverse('predict', args=[self.group.pk])
+        response = self.client.post(self.url, {'image': self.image})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_face_not_found(self):
+        # POSTリクエストの失敗テスト
+        self.url = reverse('predict', args=[self.group.pk])
+        response = self.client.post(self.url, {'image': self.image})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @patch('recognizer.service.validations.validations.is_exist_face', return_value=True)
+    def test_post_group_not_found(self):
+        # POSTリクエストの失敗テスト
+        self.url = reverse('predict', args=[uuid.uuid4()])
+        response = self.client.post(self.url, {'image': self.image})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @patch('recognizer.service.validations.validations.is_exist_face', return_value=True)
+    def test_post_feature_data_not_found(self):
+        # POSTリクエストの失敗テスト
+        self.feature_data.delete()
+        self.url = reverse('predict', args=[self.group.pk])
+        response = self.client.post(self.url, {'image': self.image})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @patch('recognizer.service.validations.validations.is_exist_face', return_value=True)
+    def test_post_another_user(self):
+        # POSTリクエストの失敗テスト
+        self.url = reverse('predict', args=[self.group.pk])
+        another_user = get_user_model().objects.create_user(
+            email='another_test_email@example.com',
+            name='another_test_user',
+            password='another_test_password',
+        )
+        self.client.force_authenticate(user=another_user)
+        response = self.client.post(self.url, {'image': self.image})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_failed(self):
         # GETリクエストの失敗テスト
