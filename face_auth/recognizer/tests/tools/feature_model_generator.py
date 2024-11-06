581f95e03602c684aa744c3a1c61de72f318c7f7
kouseidegozaru <gguangchui@gmail.com>
2024-11-06 14:31:07 +0900

    テスト用の特徴モデルの生成方法を修正

--- face_auth/recognizer/tests/tools/feature_model_generator.py
+++ face_auth/recognizer/tests/tools/feature_model_generator.py
@@ -1,14 +1,15 @@
 from ...services.recognize.feature_models import FeatureModelStorage, FeatureModel
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-import random
 
 def get_random_feature_model() -> FeatureModel:
     """
     テスト用の特徴モデルを生成
     """
+    # 二次元の仮データの作成
+    pre_features = np.array([[i + k for i in range(10)] for k in range(10)])
     pre_trained_model = NearestNeighbors()
-    pre_trained_model.fit(np.array([random.randint(0, 100)() for i in range(10)]))
+    pre_trained_model.fit(np.array(pre_features))
     pre_labels = [str(i) for i in range(10)]
     feature_model = FeatureModel(pre_trained_model, pre_labels)
     return feature_model
@@ -28,3 +29,4 @@ def convert_bynary_to_feature_model(binary_feature_model: bytes) -> FeatureModel
     feature_model_storage = FeatureModelStorage()
     feature_model_storage.set_binary_feature(binary_feature_model)
     return feature_model_storage.get()
+
