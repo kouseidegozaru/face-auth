882265cab2b4931af0b005d58fc2303ab1af334f
kouseidegozaru <gguangchui@gmail.com>
2024-11-08 11:27:57 +0900

    テスト用の画像の生成方法を変更

--- face_auth/recognizer/tests/units/test_service_Predict.py
+++ face_auth/recognizer/tests/units/test_service_Predict.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
-from recognizer.tests.tools.image_converter import SimpleUploadedFile_to_image
+from face_auth.recognizer.tests.tools.image_generator import get_test_image
 from recognizer.services.recognize.recognize import predict_feature
 from recognizer.tests.tools.feature_model_generator import get_random_feature_model
 
 class TestPredict(TestCase):
     def setUp(self):
-        image = SimpleUploadedFile("test_image.jpg", b"test_image_data", content_type="image/jpeg")
-        self.opened_image = SimpleUploadedFile_to_image(image)
+        self.opened_image = get_test_image()
 
         self.feature_model = get_random_feature_model(self.feature_model, self.opened_image)
 
