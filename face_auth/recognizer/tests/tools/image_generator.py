263a8680b4322081f1fa50ee48e0caaf671ae923
kouseidegozaru <gguangchui@gmail.com>
2024-11-09 10:05:55 +0900

    Trainの際のテスト用の画像データの作成

--- face_auth/recognizer/tests/tools/image_generator.py
+++ face_auth/recognizer/tests/tools/image_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import PIL.Image as Image
+from io import BytesIO
 
 def get_test_image() -> np.ndarray:
     """
@@ -7,3 +8,13 @@ def get_test_image() -> np.ndarray:
     """
     image = Image.new("RGB", (100, 100), color=(0, 0, 0))
     return np.array(image)
+
+def get_test_image_as_bytes() -> bytes:
+    """
+    テスト用の画像データをbytes型で返す
+    """
+    image = Image.new("RGB", (100, 100), color=(0, 0, 0))
+    buf = BytesIO()
+    image.save(buf, format='JPEG')
+
+    return buf.getvalue()
