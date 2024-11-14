0998216a35256c02d9ece44d3d212d29f91fa89e
kouseidegozaru <gguangchui@gmail.com>
2024-11-14 14:24:29 +0900

    画像の削除処理の実装

--- face_auth/recognizer/models.py
+++ face_auth/recognizer/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
 import uuid
 from accounts.models import User
+import os
 
 class TrainingGroup(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -21,6 +24,28 @@ class TrainingData(models.Model):
     label = models.CharField(verbose_name='ラベル', max_length=100)
     created_at = models.DateTimeField(verbose_name='作成日', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True)
+
+@receiver(post_delete, sender=TrainingData)
+def delete_image(sender, instance, **kwargs):
+    # 画像ファイルを削除
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+@receiver(pre_save, sender=TrainingData)
+def update_image(sender, instance, **kwargs):
+    # 画像ファイル名を更新する際に古い画像ファイルを削除
+    if not instance.pk:
+        return  # 新規インスタンスなら処理しない
+
+    try:
+        old_instance = sender.objects.get(pk=instance.pk)
+    except sender.DoesNotExist:
+        return
+
+    if old_instance.image and old_instance.image != instance.image:
+        if os.path.isfile(old_instance.image.path):
+            os.remove(old_instance.image.path)
 
 
 class FeatureData(models.Model):
