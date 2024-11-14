703c5de81ad76dae1a09ff324be5d5b45d9fdd7c
kouseidegozaru <gguangchui@gmail.com>
2024-11-14 12:04:18 +0900

    テスト画像データの作成処理を統合

--- face_auth/recognizer/tests/tools/image_generator.py
+++ face_auth/recognizer/tests/tools/image_generator.py
@@ -1,19 +1,15 @@
-import numpy as np
 import PIL.Image as Image
 from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-def get_test_image() -> np.ndarray:
-    """
-    テスト用の画像データを返す
-    """
-    image = Image.new("RGB", (100, 100), color=(0, 0, 0))
-    return np.array(image)
+def test_image() -> Image:
+    return Image.new("RGB", (100, 100), color=(0, 0, 0))
 
-def get_test_image_as_bytes() -> bytes:
+def test_image_as_bytes() -> bytes:
     """
     テスト用の画像データをbytes型で返す
     """
-    image = Image.new("RGB", (100, 100), color=(0, 0, 0))
+    image = test_image()
     buf = BytesIO()
     image.save(buf, format='JPEG')
 
