2d3cdd240043622209fbab51f933c4fd0b2ac089
kouseidegozaru <gguangchui@gmail.com>
2024-11-15 11:40:08 +0900

    メール認証のPOSTリクエストはデフォルトのものを使用するよう変更

--- face_auth/accounts/views.py
+++ face_auth/accounts/views.py
@@ -14,17 +14,6 @@ class CustomRegisterView(RegisterView):
 
 class CustomVerifyEmailView(VerifyEmailView):
     
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.kwargs['key'] = serializer.validated_data['key']
-        confirmation = self.get_object()
-        confirmation.confirm(self.request)
-        
-        # メール認証後のリダイレクト先URLを取得
-        redirect_url = settings.VERIFY_EMAIL_REDIRECT_URL
-        return HttpResponseRedirect(redirect_url)
-    
     def get(self, request, *args, **kwargs):
         # デバッグ時のみgetリクエストを許可
         if settings.DEBUG:
