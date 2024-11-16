ff631eb1437ceee2a28f0c33ef4f16c7ee77e021
kouseidegozaru <gguangchui@gmail.com>
2024-11-16 14:50:53 +0900

    TrainingGroupにswaggerの情報を追加

--- face_auth/documentation/schemas/TrainingGroup.py
+++ face_auth/documentation/schemas/TrainingGroup.py
@@ -2,7 +2,19 @@ from recognizer.views.models_views import TrainingGroupViewSet
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-#モンキーパッチでviewのメソッドを上書きする
+# warning: モンキーパッチでviewのメソッドを上書きする
 
 def update_swagger():
-    pass
+    # TrainingGroupViewSet.list
+    TrainingGroupViewSet.list = swagger_auto_schema(
+        operation_summary="TrainingGroupの一覧を取得",
+        operation_description="現在のユーザーのTrainingGroup一覧を取得します。",
+        responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
+    )(TrainingGroupViewSet.list)
+
+    # TrainingGroupViewSet.retrieve
+    TrainingGroupViewSet.retrieve = swagger_auto_schema(
+        operation_summary="特定のTrainingGroupを取得",
+        operation_description="特定のTrainingGroupを取得します。",
+        responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
+    )(TrainingGroupViewSet.retrieve)
