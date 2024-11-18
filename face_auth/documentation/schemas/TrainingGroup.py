1214b9d2a321e1f5009e820a819b2b4ddfbdceec
kouseidegozaru <gguangchui@gmail.com>
2024-11-18 11:16:54 +0900

    スキーマ情報の記載の方法の変更

--- face_auth/documentation/schemas/TrainingGroup.py
+++ face_auth/documentation/schemas/TrainingGroup.py
@@ -1,20 +1,24 @@
 from recognizer.views.models_views import TrainingGroupViewSet
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from documentation.schemas.swagger_schema_update import SwaggerSchemaUpdater
 
 # warning: モンキーパッチでviewのメソッドを上書きする
+class TrainingGroup(SwaggerSchemaUpdater):
 
-def update_swagger():
-    # TrainingGroupViewSet.list
-    TrainingGroupViewSet.list = swagger_auto_schema(
-        operation_summary="TrainingGroupの一覧を取得",
-        operation_description="現在のユーザーのTrainingGroup一覧を取得します。",
-        responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
-    )(TrainingGroupViewSet.list)
+    def run():
+        """swaggerに追加する情報を記述する"""
 
-    # TrainingGroupViewSet.retrieve
-    TrainingGroupViewSet.retrieve = swagger_auto_schema(
-        operation_summary="特定のTrainingGroupを取得",
-        operation_description="特定のTrainingGroupを取得します。",
-        responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
-    )(TrainingGroupViewSet.retrieve)
+        # TrainingGroupViewSet.list
+        TrainingGroupViewSet.list = swagger_auto_schema(
+            operation_summary="TrainingGroupの一覧を取得",
+            operation_description="現在のユーザーのTrainingGroup一覧を取得します。",
+            responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
+        )(TrainingGroupViewSet.list)
+
+        # TrainingGroupViewSet.retrieve
+        TrainingGroupViewSet.retrieve = swagger_auto_schema(
+            operation_summary="特定のTrainingGroupを取得",
+            operation_description="特定のTrainingGroupを取得します。",
+            responses={200: openapi.Response('成功'), 404: openapi.Response('Not Found')},
+        )(TrainingGroupViewSet.retrieve)
