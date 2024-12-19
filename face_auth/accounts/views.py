cdaf1283f0b361f5a0e3817375fa065eefdb5909
kouseidegozaru <gguangchui@gmail.com>
2024-12-19 09:52:29 +0900

    csrfトークンを発行するviewの作成

--- face_auth/accounts/views.py
+++ face_auth/accounts/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponseRedirect
+from rest_framework.views import APIView
 from django.conf import settings
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
 from dj_rest_auth.views import LoginView
 from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
 from .serializers import CustomLoginSerializer, CustomRegisterSerializer
@@ -20,3 +22,8 @@ class CustomVerifyEmailView(VerifyEmailView):
             return self.post(request, *args, **kwargs)
         else:
             raise MethodNotAllowed('GET')
+
+class CsrfTokenView(APIView):
+    def get(self, request, *args, **kwargs):
+        # CSRFトークンを発行してJSON形式で返す
+        return JsonResponse({'csrfToken': get_token(request)})
