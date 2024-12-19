b6f52c5b6922f79b6470be6fa6e2fd643513edd5
kouseidegozaru <gguangchui@gmail.com>
2024-12-19 15:50:16 +0900

    cookieの項目を追加

--- face_auth/documentation/schemas/TrainingData.py
+++ face_auth/documentation/schemas/TrainingData.py
@@ -36,7 +36,8 @@ class TrainingData(SwaggerSchemaUpdater):
             operation_summary="特定のTrainingDataを更新",
             operation_description="特定のTrainingDataを更新します。",
             manual_parameters=[openapi.Parameter('Authorization', openapi.IN_HEADER, description="認証トークン", type=openapi.TYPE_STRING),
-                               openapi.Parameter('X-CSRFToken', openapi.IN_HEADER, description="csrfトークン", type=openapi.TYPE_STRING),],
+                               openapi.Parameter('X-CSRFToken', openapi.IN_HEADER, description="csrfトークン", type=openapi.TYPE_STRING),
+                               openapi.Parameter('csrftoken', 'cookie', description="csrfトークン", type=openapi.TYPE_STRING),],
             request_body=openapi.Schema(
                     type=openapi.TYPE_OBJECT,
                     properties={
@@ -55,7 +56,8 @@ class TrainingData(SwaggerSchemaUpdater):
             operation_summary="特定のTrainingDataを削除",
             operation_description="特定のTrainingDataを削除します。",
             manual_parameters=[openapi.Parameter('Authorization', openapi.IN_HEADER, description="認証トークン", type=openapi.TYPE_STRING),
-                               openapi.Parameter('X-CSRFToken', openapi.IN_HEADER, description="csrfトークン", type=openapi.TYPE_STRING),],
+                               openapi.Parameter('X-CSRFToken', openapi.IN_HEADER, description="csrfトークン", type=openapi.TYPE_STRING),
+                               openapi.Parameter('csrftoken', 'cookie', description="csrfトークン", type=openapi.TYPE_STRING),],
             responses={204: openapi.Response('成功'),
                        400: openapi.Response('シリアライザーエラー'),
                        403: openapi.Response('未認証のユーザー'),
