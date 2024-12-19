530f892933142296450c0c2f9453a63c13acccf0
kouseidegozaru <gguangchui@gmail.com>
2024-12-19 09:52:53 +0900

    csrfトークンを発行するurlの設定

--- face_auth/accounts/urls.py
+++ face_auth/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from dj_rest_auth.views import PasswordResetConfirmView
-from .views import CustomLoginView, CustomRegisterView, CustomVerifyEmailView
+from .views import CustomLoginView, CustomRegisterView, CustomVerifyEmailView, CsrfTokenView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='custom_login'),
     path('registration/', CustomRegisterView.as_view(), name='custom_registration'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('registration/password/reset/confirm/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('', include('dj_rest_auth.urls')),
+    path('csrf-token/', CsrfTokenView.as_view(), name='csrf_token'),
 ]
