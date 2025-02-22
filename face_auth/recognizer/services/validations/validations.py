722291e0149332b9d35db685e1f0d15b01acb58a
kouseidegozaru <gguangchui@gmail.com>
2025-02-22 11:36:38 +0900

    画像のバリデーションチェックに特徴量が抽出できるか確認する処理を追加

--- face_auth/recognizer/services/validations/validations.py
+++ face_auth/recognizer/services/validations/validations.py
@@ -1,5 +1,5 @@
 import numpy as np
-from recognizer.services.recognize.recognize import detect_face
+from recognizer.services.recognize.recognize import detect_face,extract_face_feature
 
 
 def is_exist_face(image : np.ndarray) -> bool:
@@ -9,6 +9,9 @@ def is_exist_face(image : np.ndarray) -> bool:
     face_image = detect_face(image)
     if face_image is None:
         return False
-    else:
-        return True
+
+    face_feature = extract_face_feature(face_image)
+    if face_feature is None:
+        return False
     
+    return True
